4eb4a4acc34f0abde4a547c8a4ef353fd4eed22e
Matt Blair <elsewisemedia@gmail.com>
2013-05-30 07:35:27 -0700

    Update to guests

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,7 +2,7 @@ class GuestsController < ApplicationController
   # GET /guests
   # GET /guests.json
   def index
-    @guests = Guest.all
+    @guests = Guest.order("display_order")
 
     respond_to do |format|
       format.html # index.html.erb
