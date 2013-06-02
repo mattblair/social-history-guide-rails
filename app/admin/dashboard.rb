78c0dd7c306784589c00aa1ce375a8b79bc9f7ce
Matt Blair <elsewisemedia@gmail.com>
2013-06-01 19:23:45 -0700

    Tweaks to dashboard and nav menu

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    #div :class => "blank_slate_container", :id => "dashboard_default_message" do
+    #  span :class => "blank_slate" do
+    #    span I18n.t("active_admin.dashboard_welcome.welcome")
+    #    small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #  end
+    #end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -27,7 +27,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Stories" do
           ul do
-            Story.order("updated_at desc").limit(5).map do |story|
+            Story.order("updated_at desc").limit(10).map do |story|
               li link_to(story.title, admin_story_path(story))
             end
           end
@@ -41,5 +41,20 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
+    
   end # content
+  
+  sidebar :help do
+      ul do
+        li "Second List First Item"
+        li "Second List Second Item"
+      end
+  end
+  
+  sidebar :checklist do
+      ul do
+        li "Launch First Item"
+        li "Launch Second Item"
+      end
+  end
 end
