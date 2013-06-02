4ed64d357065be210dba5aeaf506970d6725ae47
Matt Blair <elsewisemedia@gmail.com>
2013-06-01 19:22:05 -0700

    Added comment about hiding admin users from menu

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register AdminUser do     
+  
+  # to hide the admin users completely:
+  #menu false
+  
+  # to show it only to admins:
+  #ActiveAdmin.register AdminUser do
+  #    menu :if => proc{ current_admin_user.can_edit_admin_users? }
+  #end
+  # docs:
+  # http://activeadmin.info/docs/2-resource-customization.html#customize_the_menu
+  
   index do                            
     column :email                     
     column :current_sign_in_at        
@@ -13,7 +24,7 @@ ActiveAdmin.register AdminUser do
     f.inputs "Admin Details" do       
       f.input :email                  
       f.input :password               
-      f.input :password_confirmation  
+      f.input :password_confirmation, :label => "Confirm Password (for verification)"  
     end                               
     f.actions                         
   end                                 
