bf3f6b771bd60ff1431175b67d60eefae3ae417b
Matt Blair <elsewisemedia@gmail.com>
2013-06-09 19:37:53 -0700

    Configured paper trail to use admin user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
-
+  
+  # stores id of AdminUser in PaperTrail.whodunnit, which is a string. 
+  # Use to_i to join to admin_users table
+  def user_for_paper_trail
+    return current_admin_user 
+  end
+  
 end
