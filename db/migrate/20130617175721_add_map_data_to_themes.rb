45f28d746b3f7f2d2fdc99f939dd0b1c98992af2
Matt Blair <elsewisemedia@gmail.com>
2013-06-17 11:08:14 -0700

    Added default map data type for themes

--- db/migrate/20130617175721_add_map_data_to_themes.rb
+++ db/migrate/20130617175721_add_map_data_to_themes.rb
@@ -1,7 +1,7 @@
 class AddMapDataToThemes < ActiveRecord::Migration
   def change
     add_column :themes, :map_data, :text
-    add_column :themes, :map_data_type, :integer
+    add_column :themes, :map_data_type, :integer, :default => 0
     add_column :themes, :display_in_story_list, :boolean, :default => 1
     add_column :themes, :display_in_tidbit_list, :boolean, :default => 1
   end
