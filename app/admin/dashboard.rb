eabf26b186a8b459422c2ce8a99632f092d2e53a
Matt Blair <elsewisemedia@gmail.com>
2013-06-26 06:07:42 -0700

    Added launch check list, draft stories panel to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -25,9 +25,9 @@ ActiveAdmin.register_page "Dashboard" do
 
     columns do
       column do
-        panel "Recent Stories" do
+        panel "Draft Stories" do
           ul do
-            Story.order("updated_at desc").limit(10).map do |story|
+            Story.where("workflow_state_id = 2").order("updated_at desc").limit(20).map do |story|
               li link_to(story.title, admin_story_path(story))
             end
           end
@@ -43,18 +43,29 @@ ActiveAdmin.register_page "Dashboard" do
     # end
     
   end # content
-  
+=begin
   sidebar :help do
       ul do
         li "Second List First Item"
         li "Second List Second Item"
       end
   end
-  
-  sidebar :checklist do
+=end
+
+  sidebar :launch_checklist do
       ul do
-        li "Launch First Item"
-        li "Launch Second Item"
+        li "Mastered audio files (aiff/wav)"
+        li "Cropped photos for each story, with permission for use"
+        li "Edited and proofread text for all published items"
+        li "Guest information (bio, photo, links, etc.)"
+        li "Copies of release forms for guests"
+        li "KBB interface graphics"
+        li "KBB theme graphics"
+        li "About page"
+        li "Survey questions"
+        li "Donation feature"
+        li "Proofreading of all published items"
+        li "Testing for the app"
       end
   end
 end
