13fbd409b3721a0de9fe7959795b46ce6eb113f2
Matt Blair <elsewisemedia@gmail.com>
2013-07-08 10:51:56 -0700

    Added more sidebar partials to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -27,7 +27,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Draft Stories" do
           ul do
-            Story.where("workflow_state_id = 2").order("updated_at desc").limit(20).map do |story|
+            Story.where("workflow_state_id = 2").order("updated_at desc").limit(40).map do |story|
               li link_to(story.title, admin_story_path(story))
             end
           end
@@ -43,6 +43,8 @@ ActiveAdmin.register_page "Dashboard" do
     # end
     
   end # content
+  
+  # include a sidebar directly:
 =begin
   sidebar :help do
       ul do
@@ -52,20 +54,10 @@ ActiveAdmin.register_page "Dashboard" do
   end
 =end
 
-  sidebar :launch_checklist do
-      ul do
-        li "Mastered audio files (aiff/wav)"
-        li "Cropped photos for each story, with permission for use"
-        li "Edited and proofread text for all published items"
-        li "Guest information (bio, photo, links, etc.)"
-        li "Copies of release forms for guests"
-        li "KBB interface graphics"
-        li "KBB theme graphics"
-        li "About page"
-        li "Survey questions"
-        li "Donation feature"
-        li "Proofreading of all published items"
-        li "Testing for the app"
-      end
-  end
+  # or with a partial:
+  sidebar :research_resources, :partial => "admin/resource/research_sidebar"
+  sidebar :relevant_twitter_accounts, :partial => "admin/resource/historical_twitter"
+  sidebar :other_resources, :partial => "admin/resource/other_sidebar"
+  sidebar :launch_checklist, :partial => "admin/resource/launch_checklist"
+
 end
