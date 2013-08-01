115f90f1ce78256eb1cf5efa93487418b8ddd350
Matt Blair <elsewisemedia@gmail.com>
2013-08-01 09:17:06 -0700

    Changed secret token
    And defined it outside repo, since this will eventually be public.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-KycGuideRails::Application.config.secret_token = '451a34b1b5ab86b82ad3b46b22e4453d6afdfb1294ccac2fb0ecf314e0d3d67a984076b2c7f7f1e00e55d8cd965b6655b9169d97231777454a3f3bc994ddfb64'
+
+# defined in application.yml
+KycGuideRails::Application.config.secret_token = ENV['SECRET_TOKEN']
