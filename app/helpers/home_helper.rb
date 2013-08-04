a9a8d16be8f20ba6d7ff70a5a8dcfdb24069da38
Matt Blair <elsewisemedia@gmail.com>
2013-08-04 00:14:39 -0700

    Fixed carousel indicator

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,38 +5,46 @@ module HomeHelper
     # read hash from json or yaml instead, with keys for: image, title, caption, link
     slides = %w(psych-market24 murnane-monument pdxA2004-002.692-cropped)
     
+    # this needs to use a loop, too, to handle a variable number of items
     html = <<-HTML
     <div id="home-carousel" class="carousel slide">
       <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
+    HTML
+    
+    slides.each_with_index do |slide, index|
+        
+        active_class = index == 0 ? " class=\"active\"" : ""
+        
+        html << %(<li data-target="#myCarousel" data-slide-to="#{index}"#{active_class}"></li>)
+    end
+            
+    indicator_close = <<-close  
       </ol>
       <!-- Carousel items -->
       <div class="carousel-inner">
-      HTML
+    close
       
-      slide_html =[]
+    html << indicator_close  
       
-      slides.each_with_index do |slide, index|
-        
-        active_class = index == 0 ? "active " : ""
-        
-        a_slide = <<-slide
-        <div class="#{active_class}item">
-        	<a href="/stories/the-psychedelic-supermarket">
-        	  <img alt="title" src="#{ENV['KYC_STATIC_ASSETS_URL']}#{slide}.jpg" />
-        	</a>
-    		  <div class="carousel-caption">
-              <p>Psychedelic Market</p>
-              <p>Teaser text!</p>
-    		  </div>
-        </div>
-        slide
-        
-        html << a_slide
-        
-      end
+    slides.each_with_index do |slide, index|
+      
+      active_class = index == 0 ? "active " : ""
+      
+      a_slide = <<-slide
+      <div class="#{active_class}item">
+      	<a href="/stories/the-psychedelic-supermarket">
+      	  <img alt="title" src="#{ENV['KYC_STATIC_ASSETS_URL']}#{slide}.jpg" />
+      	</a>
+  		  <div class="carousel-caption">
+            <p>Psychedelic Market</p>
+            <p>Teaser text!</p>
+  		  </div>
+      </div>
+      slide
+      
+      html << a_slide
+      
+    end
         
     slide_suffix = <<-SUFFIX    
       </div>
