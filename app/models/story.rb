f952552cd427acf0c5de2ba9cdfef79c5f4890e7
Matt Blair <elsewisemedia@gmail.com>
2013-08-10 10:39:43 -0700

    Set location_valid persistence after validation for stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -60,6 +60,7 @@ class Story < ActiveRecord::Base
   
   # make sure coordinates are not nil:
   before_validation :generate_coordinates
+  after_validation :persist_coordinate_validity
   
   def generate_coordinates
     self.latitude ||= 0.0
@@ -76,6 +77,10 @@ class Story < ActiveRecord::Base
     else
       return false
     end
+  end
+  
+  def persist_coordinate_validity
+    self.location_valid = self.valid_coordinate?
   end
   
   belongs_to :collection
@@ -95,6 +100,6 @@ class Story < ActiveRecord::Base
   scope :published, where(:workflow_state_id => 6)
   scope :testing, where(:workflow_state_id => 7)
   
-  attr_accessible :audio_filename, :audio_transcription, :display_order, :editing_priority, :editorial_notes, :image_credit, :image_name, :keywords, :latitude, :longitude, :original_audio_filename, :audio_filename, :subtitle, :summary, :thumbnail_name, :title, :twitter_template, :collection_id, :theme_id, :guest_id, :media_type_id, :workflow_state_id, :image_credit_url, :image_copyright_notice, :image_copyright_url, :image_copyright_details, :slug, :photo_notes, :proofreader_id, :more_info_url, :more_info_title, :more_info_description, :more_info_notes
+  attr_accessible :audio_filename, :audio_transcription, :display_order, :editing_priority, :editorial_notes, :image_credit, :image_name, :image_caption, :keywords, :latitude, :longitude, :original_audio_filename, :audio_filename, :subtitle, :summary, :thumbnail_name, :title, :twitter_template, :collection_id, :theme_id, :guest_id, :media_type_id, :workflow_state_id, :image_credit_url, :image_copyright_notice, :image_copyright_url, :image_copyright_details, :slug, :photo_notes, :proofreader_id, :more_info_url, :more_info_title, :more_info_description, :more_info_notes, :location_valid
 
 end
