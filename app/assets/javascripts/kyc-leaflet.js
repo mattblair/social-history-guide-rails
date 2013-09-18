8b3a83b13efd39af10ce55038bb101b331e2a805
Matt Blair <elsewisemedia@gmail.com>
2013-09-17 23:49:28 -0700

    Tweaking of pin position and popup offset

--- app/assets/javascripts/kyc-leaflet.js
+++ app/assets/javascripts/kyc-leaflet.js
@@ -30,9 +30,9 @@ function addMarkerToMap(lat, lng, map) {
 
 	    iconSize:     [15, 36], // actual size: 15, 56
 	    shadowSize:   [10, 10], // was 50, 64
-	    iconAnchor:   [11, 29], // point of the icon which will correspond to marker's location
-	    shadowAnchor: [4, 62],  // the same for the shadow
-	    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+	    iconAnchor:   [7, 29], // point of the icon which will correspond to marker's location
+	    shadowAnchor: [4, 62], // the same for the shadow
+	    popupAnchor:  [1, -28] // point from which the popup should open relative to the iconAnchor
 	});
 	
 	L.marker([lat, lng], {icon: thinPin}).addTo(map);
@@ -91,9 +91,9 @@ function addGeoJSONToMap(geojson, map) {
 	
 	    iconSize:     [15, 36], // actual size: 15, 56
 	    shadowSize:   [10, 10], // was 50, 64
-	    iconAnchor:   [11, 29], // point of the icon which will correspond to marker's location
+	    iconAnchor:   [7, 29], // point of the icon which will correspond to marker's location
 	    shadowAnchor: [4, 62],  // the same for the shadow
-	    popupAnchor:  [-6, -45] // point from which the popup should open relative to the iconAnchor
+	    popupAnchor:  [1, -28] // point from which the popup should open relative to the iconAnchor
 	});
 
 	function onEachFeature(feature, layer) {
